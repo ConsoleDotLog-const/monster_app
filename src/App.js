729adbc99f94b44b4a4c6a7c2b1fa473fb6f6d9e
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-09-20 19:18:01 +1200

    final

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 import { CardList } from './components/card-list/card-list'
 import  { SearchBox } from './components/search-box/search-box'
 
-
-
-
 class App extends Component {
 
   constructor(){
@@ -34,6 +31,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monsters</h1>
        <SearchBox
           placeholder= "Search Monsters"
           handleChange ={e => this.setState({searchField: e.target.value})}
--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../../index.css'
 
  export const SearchBox = ({ placeholder, handleChange}) =>(
-    <input type="search" 
+  
+    <input 
+        className="search"
+        type="search" 
         placeholder= {placeholder}
         onChange={handleChange}
         />
+    
 )
 
