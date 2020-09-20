6d63721fbb761ee1494df4a3e79a57097c303c83
ConsoleDotLog-const <c-gusto@hotmail.com>
2020-09-20 17:44:44 +1200

    searchField completed, creating a search field component

--- src/App.js
+++ src/App.js
@@ -27,10 +27,14 @@ class App extends Component {
  
  
   render(){
+    const { monsters,searchField } = this.state;
+    const filteredMonsters = monsters.filter(monsters =>
+      monsters.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
+
     return (
       <div className="App">
         <input type="search" placeholder="Search Monster" onChange={e => this.setState({searchField : e.target.value})}/>
-        <CardList monsters ={this.state.monsters}/>
+        <CardList monsters ={filteredMonsters}/>
       
       </div>
      
